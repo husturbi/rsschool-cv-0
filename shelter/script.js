7ba4082680729fc6c02fc8449f6c2cce53147cac
husturbi <rotes@yandex.ru>
2024-08-21 01:43:28 +0700

    feat: add burger-menu

--- shelter/script.js
+++ shelter/script.js
@@ -1,10 +1,24 @@
-// плавный скролл по якорям
-/*document.querySelectorAll('a[href^="#"]').forEach(anchor =&gt; {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+const menuBtn = document.querySelector('.menu-btn');
+const navbarList = document.querySelector('.navbar-list');
+const hiddenOverley = document.querySelector('.hidden-overley');
+const bodyNoScroll = document.querySelector('body');
+const navLink = document.getElementsByClassName('navbar-link');
  
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});*/
+/*Функция добавления/удаления классов при нажатии*/
+const pushLinkFunction = function() {
+    menuBtn.classList.toggle('open');
+    navbarList.classList.toggle('navbar-list-open');
+    hiddenOverley.classList.toggle('navbar-list-open');
+    bodyNoScroll.classList.toggle('navbar-list-open');
+};
+
+/*Событие на бургер-меню*/
+menuBtn.addEventListener('click', pushLinkFunction, false);
+
+/*Событие на оверлей*/
+hiddenOverley.addEventListener('click', pushLinkFunction, false);
+
+/*Событие на ссылки меню(их несколько)*/
+for( i=0; i < navLink.length; i++ ){
+    navLink[i].addEventListener('click', pushLinkFunction, false);
+}
